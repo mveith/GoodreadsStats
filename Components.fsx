6abd2057783ea6831120a627efa776ee50f4dae2
mveith <mveith@hotmail.cz>
2017-01-21 18:04:09 +0100

    Refactoring - Redux in separated file

--- Components.fsx
+++ Components.fsx
@@ -5,37 +5,7 @@
 #load "Model.fsx"
 #load "Utils.fsx"
 #load "BasicStatsCalculator.fsx"
-module Redux =
-    open System
-    open Fable.Import
-    open Fable.Core
-    open Fable.Core.JsInterop
-
-    type IStore<'TState, 'TAction> = interface end
-
-    let [<Import("createStore","redux")>] private createStore' = obj()
-
-    let createStore (reducer: 'TState->'TAction->'TState) (initState: 'TState) =
-        // Check if the action is a union type before applying the reducer
-        let reducer = fun state action ->
-            match box action?Case with
-            | :? string -> reducer state action
-            | _ -> state
-        match unbox Browser.window?devToolsExtension with
-        | Some ext -> createStore'$(Func<_,_,_> reducer, initState, ext$())
-        | None -> createStore'$(Func<_,_,_> reducer, initState)
-        |> unbox<IStore<'TState, 'TAction>>
-
-    let dispatch (store: IStore<'TState, 'TAction>) (x: 'TAction) =
-        let x = toPlainJsObj x
-        x?``type`` <- x?Case
-        store?dispatch(x) |> ignore
-
-    let inline subscribe (store: IStore<'TState, 'TAction>) (f: unit->unit) =
-        store?subscribe(f) |> ignore
-
-    let inline getState (store: IStore<'TState, 'TAction>) =
-        store?getState() |> unbox<'TState>
+#load "Fable.Import.Redux.fsx"
 
 open Fable.Import
 open Fable.Import.Global
@@ -46,6 +16,7 @@ open Fable.Import
 module R = Fable.Helpers.React
 open R.Props
 open BasicStatsCalculator
+open Fable.Import.Redux
 
 [<Pojo>]
 type AccessTokenData = 
---
+++ Fable.Import.Redux.fsx
@@ -0,0 +1,34 @@
+﻿#r "node_modules/fable-core/Fable.Core.dll"
+
+#load "Fable.Import.Global.fsx"
+
+open System
+open Fable.Core
+open Fable.Core.JsInterop
+
+type IStore<'TState, 'TAction> = interface end
+
+let [<Import("createStore","redux")>] private createStore' = obj()
+
+let createStore (reducer: 'TState->'TAction->'TState) (initState: 'TState) =
+    // Check if the action is a union type before applying the reducer
+    let reducer = fun state action ->
+        match box action?Case with
+        | :? string -> reducer state action
+        | _ -> state
+    match unbox Browser.window?devToolsExtension with
+    | Some ext -> createStore'$(Func<_,_,_> reducer, initState, ext$())
+    | None -> createStore'$(Func<_,_,_> reducer, initState)
+    |> unbox<IStore<'TState, 'TAction>>
+
+let dispatch (store: IStore<'TState, 'TAction>) (x: 'TAction) =
+    let x = toPlainJsObj x
+    x?``type`` <- x?Case
+    store?dispatch(x) |> ignore
+
+let inline subscribe (store: IStore<'TState, 'TAction>) (f: unit->unit) =
+    store?subscribe(f) |> ignore
+
+let inline getState (store: IStore<'TState, 'TAction>) =
+    store?getState() |> unbox<'TState>
+
